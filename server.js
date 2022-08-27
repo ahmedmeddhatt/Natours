62b7345e62a01c0c2bc47c8992c06a568fd88065
Ahmed Medhat <ahmedmedhat1231@gmail.com>
2022-08-27 20:52:18 +0200

    updating config.env

--- server.js
+++ server.js
@@ -11,16 +11,14 @@ process.on('uncaughtException', err => {
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
 
 mongoose
   .connect(DB, {
     useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+    useNewUrlParser: true
   })
-  .then(() => console.log('DB connection successful!'))
+  .then(() => console.log('DB connection successful 🙏'))
   .catch(err => console.log(chalk.redBright(err)));
 
 const port = process.env.PORT || 3000;
